a9fb9ee142c83d309d748928f32d25cdf401cb3b
mac <123>
2017-10-09 16:55:20 +0800

    提交框架

--- wine/app/src/androidTest/java/zjw/wine/ExampleInstrumentedTest.java
+++ wine/app/src/androidTest/java/zjw/wine/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package zjw.wine;
+package com;
 
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("zjw.wine", appContext.getPackageName());
+        assertEquals("com", appContext.getPackageName());
     }
 }
--- wine/app/src/main/java/com/activity/MainActivity.java
+++ wine/app/src/main/java/com/activity/MainActivity.java
@@ -1,7 +1,9 @@
-package zjw.wine;
+package com.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import zjw.wine.R;
 
 public class MainActivity extends AppCompatActivity {
 
